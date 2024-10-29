e5d31ac295d4b68a1603d3975a2238703b337f84
dl364823 <146682379+dl364823@users.noreply.github.com>
2024-10-28 20:51:05 -0400

    add CORS cofiguraton

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.config.log import setup_logger
 from app.dependecies.logging_middleware import logging_dependency
 from app.service.order_service import order_router
@@ -6,6 +7,14 @@ from app.service.order_service import order_router
 logger = setup_logger()
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  
+    allow_credentials=True,
+    allow_methods=["*"],   
+    allow_headers=["*"],  
+)
 
 app.middleware("http")(logging_dependency)
 
