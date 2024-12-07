b062c80b4839faf68e9516ec678d3541ded4cb0d
hxyin <yinhaoxiang42@outlook.com>
2024-12-07 12:23:47 -0500

    log_service_to_cloudwatch

--- app/main.py
+++ app/main.py
@@ -1,12 +1,11 @@
 from fastapi import FastAPI
-from app.config.log import setup_logger
-from app.dependecies.logging_middleware import logging_dependency
+from app.config.cloudwatch_logger import setup_cloudwatch_logger
+from app.dependencies.logging_middleware import logging_dependency
 from app.service.order_service import order_router
 
-logger = setup_logger()
+service_name = "order-service-8004"
+logger = setup_cloudwatch_logger(service_name)
 
 app = FastAPI()
-
 app.middleware("http")(logging_dependency)
-
 app.include_router(order_router)
